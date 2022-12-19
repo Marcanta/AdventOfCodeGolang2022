79bdba47f76e0d88443b6c49b5c47adb527edbac
Marcant Aimé <aime.marcant@hotmail.fr>
2022-12-19 14:25:00 +0100

    Finish day 9

--- src/day-9/09-rope-bridge.go
+++ src/day-9/09-rope-bridge.go
@@ -20,13 +20,13 @@ const (
 	Left            = "L"
 )
 
-type Vertex struct {
+type Knot struct {
 	X, Y int
 }
 
 type Rope struct {
-	head        Vertex
-	tail        Vertex
+	head        Knot
+	tail        []Knot
 	tailVisited map[string]bool
 }
 
@@ -38,30 +38,33 @@ func readFile(path string) string {
 	return string(data)
 }
 
-func (r *Rope) moveTail(direction Direction) {
-	switch direction {
-	case Up:
-		r.tail = Vertex{X: r.head.X, Y: r.head.Y - 1}
-	case Down:
-		r.tail = Vertex{X: r.head.X, Y: r.head.Y + 1}
-	case Right:
-		r.tail = Vertex{X: r.head.X - 1, Y: r.head.Y}
-	case Left:
-		r.tail = Vertex{X: r.head.X + 1, Y: r.head.Y}
+func moveTail(head Knot, tail Knot) (newTail Knot) {
+	switch xDiff := head.X - tail.X; {
+	case xDiff > 1:
+		newTail = Knot{X: head.X - 1, Y: head.Y}
+	case xDiff < -1:
+		newTail = Knot{X: head.X + 1, Y: head.Y}
 	}
-	r.tailVisited[fmt.Sprintf("%s,%s", r.tail.X, r.tail.Y)] = true
+
+	switch yDiff := head.Y - tail.Y; {
+	case yDiff > 1:
+		newTail = Knot{X: head.X, Y: head.Y - 1}
+	case yDiff < -1:
+		newTail = Knot{X: head.X, Y: head.Y + 1}
+	}
+	return
 }
 
 func (r *Rope) moveHead(direction Direction) {
 	switch direction {
 	case Up:
-		r.head = Vertex{X: r.head.X, Y: r.head.Y + 1}
+		r.head = Knot{X: r.head.X, Y: r.head.Y + 1}
 	case Down:
-		r.head = Vertex{X: r.head.X, Y: r.head.Y - 1}
+		r.head = Knot{X: r.head.X, Y: r.head.Y - 1}
 	case Right:
-		r.head = Vertex{X: r.head.X + 1, Y: r.head.Y}
+		r.head = Knot{X: r.head.X + 1, Y: r.head.Y}
 	case Left:
-		r.head = Vertex{X: r.head.X - 1, Y: r.head.Y}
+		r.head = Knot{X: r.head.X - 1, Y: r.head.Y}
 	}
 }
 
@@ -71,9 +74,15 @@ func (r *Rope) moveRope(instruction string) {
 	number, _ := strconv.Atoi(instructionSplitted[1])
 	for i := 0; i < number; i++ {
 		r.moveHead(direction)
-		if math.Abs(float64(r.head.X)-float64(r.tail.X)) > 1 || math.Abs(float64(r.head.Y)-float64(r.tail.Y)) > 1 {
-			r.moveTail(direction)
+		if math.Abs(float64(r.head.X)-float64(r.tail[0].X)) > 1 || math.Abs(float64(r.head.Y)-float64(r.tail[0].Y)) > 1 {
+			r.tail[0] = moveTail(r.head, r.tail[0])
 		}
+		for i := 1; i < len(r.tail); i++ {
+			if math.Abs(float64(r.tail[i-1].X)-float64(r.tail[i].X)) > 1 || math.Abs(float64(r.tail[i-1].Y)-float64(r.tail[i].Y)) > 1 {
+				r.tail[i] = moveTail(r.tail[i-1], r.tail[i])
+			}
+		}
+		r.tailVisited[fmt.Sprintf("%d,%d", r.tail[len(r.tail)-1].X, r.tail[len(r.tail)-1].Y)] = true
 	}
 }
 
@@ -82,10 +91,20 @@ func main() {
 
 	data := strings.Split(rawData, "\n")
 
-	rope := Rope{head: Vertex{X: 0, Y: 0}, tail: Vertex{X: 0, Y: 0}, tailVisited: map[string]bool{"0,0": true}}
+	// Part 1
+	shortRope := Rope{head: Knot{X: 0, Y: 0}, tail: []Knot{{X: 0, Y: 0}}, tailVisited: map[string]bool{"0,0": true}}
 	for _, instruction := range data {
-		rope.moveRope(instruction)
+		shortRope.moveRope(instruction)
 	}
 
-	fmt.Printf("rope: %v, nbVisited: %d", rope, len(rope.tailVisited))
+	fmt.Printf("ShortRope: , nbVisited: %d", len(shortRope.tailVisited))
+
+	// Part 2
+	longRope := Rope{head: Knot{X: 0, Y: 0}, tail: make([]Knot, 10), tailVisited: map[string]bool{"0,0": true}}
+	for _, instruction := range data {
+		longRope.moveRope(instruction)
+	}
+
+	fmt.Printf("rope:, nbVisited: %d", len(longRope.tailVisited))
+
 }
